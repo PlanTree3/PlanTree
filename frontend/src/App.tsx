07c5c9a8009817428b1e08a1554e388484aed10a
신기정 <467shin@gmail.com>
2023-10-23 16:03:06 +0900

    fix: ESLint로 인한 코드 수정 방지를 목적으로 App component내부의 변수 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
 import logo from '../public/plantree.png';
 
-const App = () => (
-  <>
-    <img className="logo" src={logo} alt="a" />
-    <h1>Hey!</h1>
-  </>
-);
-
+const App = () => {
+  // 이거 안 하면 린트가 지 맘대로 중괄호랑 리턴이랑 다 죽여버려서
+  // 리턴 밖에 변수 하나 해두었습니다
+  const a = logo;
+  return (
+    <>
+      <img className="logo" src={a} alt="a" />
+      <h1>Hey!</h1>
+    </>
+  );
+};
 export default App;
