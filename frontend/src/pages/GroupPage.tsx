877b45933284ad1684a0d1190c2ead661fc8aa41
김연재 <yjkim9195@gmail.com>
2023-10-27 17:46:15 +0900

    feat: groupPage-student 임시 레이아웃

---
+++ frontend/src/pages/GroupPage.tsx
@@ -0,0 +1,33 @@
+import chick from '../../public/chick.png'
+import forest from '../../public/forest_tmp.png'
+
+const GroupStudentPage = () => {
+  <>
+
+  </>
+  return (
+    <>
+    <text>내 둥지 확인하기</text>
+    <div className="box-border h-30 w-2/3 p-5 border-4 bg-amber-700 rounded-3xl">
+      <div className="flex flex-row">
+        <img className="chick flex flex-start"src={chick} alt=""/>
+        <div className="flex items-center text-white tracking-widest">
+          여기는 그룹이름임
+          <br/>
+          둥지장동지:
+          <br/>
+          병아리 동지:
+        </div>
+      </div>
+    </div> 
+    <text>내 그룹 확인하기</text>
+    <div className="flex flex-row">
+    <img className="forest"src={forest} alt="" />
+    <img className="forest"src={forest} alt="" />
+    <img className="forest"src={forest} alt="" />
+    </div>
+    </>
+  )
+  }
+
+  export default GroupStudentPage
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.tsx'
 import LoginPage from './pages/LoginPage.tsx'
 import OauthRedirectPage from './pages/OauthRedirectPage.tsx'
+import GroupStudentPage from './pages/GroupPage.tsx'
 
 const RoutesComponent = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/oauth" element={<OauthRedirectPage />} />
+    <Route path="/groupStudent" element={<GroupStudentPage />} />
   </Routes>
 )
 
