6241881aa2c9dc383d76e1eaac86caf82c7b629a
신기정 <467shin@gmail.com>
2023-10-30 17:23:28 +0900

    fix: build error

--- frontend/src/stores/services/rootSaga.ts
+++ frontend/src/stores/services/rootSaga.ts
@@ -1,7 +1,7 @@
 import { all } from 'redux-saga/effects'
-import watchFetchUserData from '@/stores/services/userSaga'
+// import watchFetchUserData from '@/stores/services/userSaga'
 import { watchTokenEvent } from '@/stores/services/tokenEventSaga'
 
 export default function* rootSaga() {
-  yield all([watchFetchUserData(), watchTokenEvent()])
+  yield all([watchTokenEvent()])
 }
