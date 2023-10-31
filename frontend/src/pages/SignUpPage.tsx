c896824e7e0e7b18c2ab389cc8d51b9cfc71dac4
정예지 <wjddpwl1222@gmail.com>
2023-10-31 17:05:45 +0900

    [S09P31A302-191] feat: Check  UserName Length

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,18 +1,39 @@
-import React, { useState } from 'react'
-import { debounce } from 'redux-saga/effects'
+import React, { ChangeEvent, useState } from 'react'
 
 const SignUpPage = () => {
   const [inputName, setInputName] = useState<string>('')
   const [inputCount, setInputCount] = useState<number>(0)
 
-  // debounce 함수를 사용하여 GET 요청을 보내는 함수
-  const debounceSearch = debounce(async (name: string) => {
+  // 이름이 10글자를 넘어갈 때 스와이프 안되게 하는 함수
 
-    if(name.length > 0) {
-      const response = await a
+  // 이름 세는 함수
+  const onInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    if (value.length >= 0 && value.length <= 10) {
+      setInputName(value)
+      setInputCount(value.length)
     }
-  })
-  return <div>저는 이런 사람입니다!</div>
+    setInputCount(value.length)
+  }
+
+  console.log(setInputName)
+
+  return (
+    <>
+      <div>저는 이런 사람입니다!</div>
+      <input type="text" value={inputName} onChange={onInputHandler} />
+      <div>
+        {inputCount <= 10 ? (
+          <>
+            <span>{inputCount}</span>
+            <span>/10</span>
+          </>
+        ) : (
+          <span>이름은 10자를 넘을 수 없습니다.</span>
+        )}
+      </div>
+    </>
+  )
 }
 
 export default SignUpPage
