84f272de807feb2c6ee014ea15cf512545ef7f43
정예지 <wjddpwl1222@gmail.com>
2023-10-31 15:27:38 +0900

    [S09P31A302-192] feat: SignUp api 연결중

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,5 +1,18 @@
+import React, { useState } from 'react'
+import { debounce } from 'redux-saga/effects'
+
 const SignUpPage = () => {
-  return <div>SignUp Page입니다.</div>
+  const [inputName, setInputName] = useState<string>('')
+  const [inputCount, setInputCount] = useState<number>(0)
+
+  // debounce 함수를 사용하여 GET 요청을 보내는 함수
+  const debounceSearch = debounce(async (name: string) => {
+
+    if(name.length > 0) {
+      const response = await a
+    }
+  })
+  return <div>저는 이런 사람입니다!</div>
 }
 
 export default SignUpPage
