bd6cf6c293831fb8c624f376252a36ba2725a3c3
신기정 <467shin@gmail.com>
2023-10-31 17:56:11 +0900

    feat: api 수정 완

--- frontend/src/apis/communication/notice.ts
+++ frontend/src/apis/communication/notice.ts
@@ -3,6 +3,12 @@ import { authApi } from '@/apis'
 const baseUrl = 'api/communication-service'
 
 // 가정통신문 생성
+const groupNoticeCreate = async (groupId: number, data: unknown) => {
+  return authApi
+    .post(`${baseUrl}/group/${groupId}/notice`, data)
+    .then((res) => res)
+    .catch((err) => err)
+}
 
 // 가정통신문 리스트 조회(학생, 부모)
 const noticeList = async () => {
@@ -28,4 +34,22 @@ const groupNoticeList = async (groupId: number) => {
     .catch((err) => err)
 }
 
-export { noticeList, noticeDetail, groupNoticeList }
+// 가정통신문 수정
+const groupNoticeUpdate = async (
+  groupId: number,
+  noticeId: number,
+  data: unknown,
+) => {
+  return authApi
+    .patch(`${baseUrl}/group/${groupId}/notice/${noticeId}`, data)
+    .then((res) => res)
+    .catch((err) => err)
+}
+
+export {
+  groupNoticeCreate,
+  noticeList,
+  noticeDetail,
+  groupNoticeList,
+  groupNoticeUpdate,
+}
