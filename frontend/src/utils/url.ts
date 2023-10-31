fc8095a96ff437424ad4862b1b8598d5cf641af1
신기정 <467shin@gmail.com>
2023-10-31 10:35:00 +0900

    feat: communication API

--- frontend/src/utils/url.ts
+++ frontend/src/utils/url.ts
@@ -1,30 +1,24 @@
 const baseUrl = 'api/forest-service/forest'
 
 // treeId로 끝나는 api url을 문자열 형태로 반환하는 함수
-const treeApiUrl = (forestId: number, treeId: number) => {
-  return `${baseUrl}/${forestId}/tree/${treeId}`
-}
+const treeApiUrl = (forestId: number, treeId: number) =>
+  `${baseUrl}/${forestId}/tree/${treeId}`
 
-const branchApiUrl = (forestId: number, treeId: number, branchId: number) => {
-  return `${baseUrl}/${forestId}/tree/${treeId}/branch/${branchId}`
-}
+const branchApiUrl = (forestId: number, treeId: number, branchId: number) =>
+  `${baseUrl}/${forestId}/tree/${treeId}/branch/${branchId}`
 
 const seedApiUrl = (
   forestId: number,
   treeId: number,
   branchId: number,
   seedId: number,
-) => {
-  return `${baseUrl}/${forestId}/tree/${treeId}/branch/${branchId}/seed/${seedId}`
-}
+) => `${baseUrl}/${forestId}/tree/${treeId}/branch/${branchId}/seed/${seedId}`
 
 const budApiUrl = (
   forestId: number,
   treeId: number,
   branchId: number,
   budId: number,
-) => {
-  return `${baseUrl}/${forestId}/tree/${treeId}/branch/${branchId}/bud/${budId}`
-}
+) => `${baseUrl}/${forestId}/tree/${treeId}/branch/${branchId}/bud/${budId}`
 
 export { treeApiUrl, branchApiUrl, seedApiUrl, budApiUrl }
