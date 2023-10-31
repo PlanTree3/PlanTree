d60afc2088c0c6f9ee2a5ef5e32a143ae2590afc
김연재 <yjkim9195@gmail.com>
2023-10-31 17:49:25 +0900

    [S09P31A302-214]feat: 배경 수정, 그룹페이지 진행 중

--- frontend/src/pages/AdminGroupPage.tsx
+++ frontend/src/pages/AdminGroupPage.tsx
@@ -1,12 +1,16 @@
 // import React, { useEffect, useState } from 'react';
 // import axios from 'axios';
+import './GroupPage.css'
 
 const AdminGroupPage = () => {
-<>
-</>
-return (
-
-)}
-
+  return (
+    <div className="whiteBox">
+      <text>2023 3학년 1반</text>
+      <div className="studentListBox">
+        <text>번호 학생이름 이번주달성도</text>
+      </div>
+    </div>
+  )
+}
 
 export default AdminGroupPage
--- frontend/src/pages/StudentGroupPage.tsx
+++ frontend/src/pages/StudentGroupPage.tsx
@@ -2,9 +2,48 @@
 
 import chick from '../../public/chick.png'
 import forest from '../../public/forest_tmp.png'
-import './StudentGroupPage.css'
+import './GroupPage.css'
 // import axios from 'axios';
 // import StudentGroupPageResponse from '../types/GroupStudentType'
+
+// const tmpData = [
+//   {
+//     studentId: 1,
+//     studentName: '학생 1',
+//     totalBudCount: 20,
+//     completedBudCount: 15,
+//   },
+//   {
+//     studentId: 2,
+//     studentName: '학생 2',
+//     totalBudCount: 30,
+//     completedBudCount: 10,
+//   },
+//   {
+//     studentId: 3,
+//     studentName: '학생 3',
+//     totalBudCount: 25,
+//     completedBudCount: 20,
+//   },
+//   {
+//     studentId: 4,
+//     studentName: '학생 4',
+//     totalBudCount: 15,
+//     completedBudCount: 10,
+//   },
+//   {
+//     studentId: 5,
+//     studentName: '학생 5',
+//     totalBudCount: 40,
+//     completedBudCount: 35,
+//   },
+//   {
+//     studentId: 6,
+//     studentName: '학생 6',
+//     totalBudCount: 22,
+//     completedBudCount: 17,
+//   },
+// ]
 
 const StudentGroupPage = () => {
   // const [data, setData] = useState<StudentGroupPageResponse | null>(null);
@@ -23,25 +62,30 @@ const StudentGroupPage = () => {
 
   return (
     // <div className="sketchbook">
-    <div className="whiteBox">
-      <text>내 둥지 확인하기</text>
-      <div className="box-border h-30 w-2/3 p-5 border-4 bg-amber-700 rounded-3xl">
-        <div className="flex flex-row">
-          <img className="chick flex flex-start" src={chick} alt="" />
-          <div className="flex items-center text-white tracking-widest">
-            여기는 그룹이름임
-            <br />
-            둥지장동지:
-            <br />
-            병아리 동지:
+    <div className="p-10">
+      <div className="whiteBox pb-5">
+        <text>내 둥지 확인하기</text>
+        <div className="box-border h-30 w-2/3 p-5 border-4 bg-amber-700 rounded-3xl">
+          <div className="flex flex-row">
+            <img className="chick flex flex-start" src={chick} alt="" />
+            <div className="flex items-center text-white tracking-widest">
+              여기는 그룹이름임
+              <br />
+              둥지장동지:
+              <br />
+              병아리 동지:
+            </div>
           </div>
         </div>
-      </div>
-      <text>내 그룹 확인하기</text>
-      <div className="flex flex-row">
-        <img className="forest" src={forest} alt="" />
-        <img className="forest" src={forest} alt="" />
-        <img className="forest" src={forest} alt="" />
+        <text>내 그룹 확인하기</text>
+        <div className="flex-container">
+          <img className="forest" src={forest} alt="" />
+          <img className="forest" src={forest} alt="" />
+          <img className="forest" src={forest} alt="" />
+          <img className="forest" src={forest} alt="" />
+          <img className="forest" src={forest} alt="" />
+          <img className="forest" src={forest} alt="" />
+        </div>
       </div>
     </div>
   )
