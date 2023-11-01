482308f3e885ea9cb8addfdd2c8f7ce128367f4e
정예지 <wjddpwl1222@gmail.com>
2023-11-01 14:17:19 +0900

    [S09P31A302-194] feat: Add SignUp Swiper

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,28 +1,38 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react'
 // import required modules
-import { Pagination } from 'swiper/modules'
+import { Mousewheel, Pagination } from 'swiper/modules'
 import { UserBirth, UserName, UserProfileImg, UserRule } from '@/components'
+// style
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+import './SlideStyle.css'
 
 const SignUpPage = () => {
   return (
-    <>
-      <Swiper pagination modules={[Pagination]} className="mySwiper">
-        <SwiperSlide>
-          <UserName />
-        </SwiperSlide>
-        <SwiperSlide>
-          <UserBirth />
-        </SwiperSlide>
-        <SwiperSlide>
-          <UserRule />
-        </SwiperSlide>
-        <SwiperSlide>
-          <UserProfileImg />
-        </SwiperSlide>
-      </Swiper>
-      <div>sdfsf</div>
-    </>
+    <Swiper
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      mousewheel
+      keyboard
+      modules={[Mousewheel, Pagination]}
+      className="mySwiper"
+      style={{ height: '100px' }}
+    >
+      <SwiperSlide>
+        <UserName />
+      </SwiperSlide>
+      <SwiperSlide>
+        <UserBirth />
+      </SwiperSlide>
+      <SwiperSlide>
+        <UserRule />
+      </SwiperSlide>
+      <SwiperSlide>
+        <UserProfileImg />
+      </SwiperSlide>
+    </Swiper>
   )
 }
 
