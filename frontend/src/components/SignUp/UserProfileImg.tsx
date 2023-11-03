13d5d4d23574b2503aa49a5eede404123e6ab38b
정예지 <wjddpwl1222@gmail.com>
2023-11-03 17:43:25 +0900

    [S09P31A302-196] feat: Add Modal in UserProfileImg Page

--- frontend/src/components/SignUp/UserProfileImg.tsx
+++ frontend/src/components/SignUp/UserProfileImg.tsx
@@ -1,19 +1,43 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './SignUp.scss'
-import { userLogin } from '@/apis/member'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { userSignup } from '@/apis/member'
 
 const UserProfileImg = () => {
-  // const [inputProfileImg, setInputProfileImg] = useState<string>('')
-  // const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [inputProfileImg, setInputProfileImg] = useState<string>('')
+  const MySwal = withReactContent(Swal)
 
   const saveUser = () => {
     // 임시로 userLogin 호출
-    userLogin('이것은 임시입니다')
+    userSignup('이것은 임시입니다')
+  }
+
+  const chooseProfileImg = (url: string) => {
+    setInputProfileImg(url)
+  }
+
+  const moveProfileImg = () => {
+    const content = (
+      <button onClick={() => chooseProfileImg('src/asset/profile/bear.jpg')}>
+        <img src="src/asset/profile/bear.jpg" alt="bear" />
+      </button>
+    )
+
+    MySwal.fire({
+      title: '당신의 원픽은?',
+      html: content,
+      width: 600,
+    })
   }
 
   return (
     <div className="w-8/12 h-3/5 relative">
       <div className="bg-no-repeat w-full h-full bg-contain bg-[url('./asset/student_card/rm245-bb-17-g.jpg')]">
+        <button onClick={() => moveProfileImg()}>
+          {/* <input type="file" accept="image/*" onChange={handleFileUpload} /> */}
+          모달 띄웁니다!
+        </button>
         <div className="rounded-full border-slate-950">학생</div>
         <div>이름</div>
         <div>나이</div>
