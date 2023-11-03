e9b1e56116765d5be95c79bb2af8e2f72ac2e1bc
정예지 <wjddpwl1222@gmail.com>
2023-11-03 10:03:27 +0900

    [S09P31A302-196] feat: Add Modal in UserProfileImg Page

--- frontend/src/components/SignUp/UserProfileImg.tsx
+++ frontend/src/components/SignUp/UserProfileImg.tsx
@@ -1,5 +1,38 @@
+// import { useState } from 'react'
+import { ReactDOM } from 'react-dom'
+import Modal from 'react-modal'
+
 const UserProfileImg = () => {
-  return <div>UserProfileImg Page입니다.</div>
+  const [inputProfileImg, setInputProfileImg] = useState<string>('')
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  }
+
+  const openModal = () => {
+    setModalIsOpen(true)
+  }
+
+  const closeModal = () => [setModalIsOpen(false)]
+  return (
+    <>
+      <div>
+        <button onClick={openModal}>모달 열기</button>
+        <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+          <h2>모달 제목</h2>
+          <button onClick={closeModal}>닫기</button>
+        </Modal>
+      </div>
+    </>
+  )
 }
 
 export default UserProfileImg
