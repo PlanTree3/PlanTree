0db934a048364e74512737fbfd853314200fe1db
정예지 <wjddpwl1222@gmail.com>
2023-11-05 18:10:46 +0900

    [S09P31A302-197] feat: Connect api of SignUp

--- frontend/src/components/RenewalGoogleLogin.tsx
+++ frontend/src/components/RenewalGoogleLogin.tsx
@@ -1,14 +1,28 @@
 import { useGoogleLogin } from '@react-oauth/google'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 import googleBtn from '../asset/login_btn/web_light_sq_na@4x.png'
-// import axios from 'axios'
+import { userLogin } from '@/apis/member'
+import { addOauthProvider } from '@/stores/features/signupSlice'
 
 const RenewalGoogleLogin = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const googleSocialLogin = useGoogleLogin({
-    onSuccess: (codeResponse) => {
+    onSuccess: async (codeResponse) => {
       console.log(codeResponse)
       const data = {
         oauthProvider: 'GOOGLE',
         idToken: codeResponse,
+      }
+
+      const loginResult = await userLogin(data)
+
+      if (loginResult) {
+        dispatch(addOauthProvider('GOOGLE'))
+        navigate('/signUp')
+      } else {
+        navigate('/main')
       }
     },
     flow: 'auth-code',
