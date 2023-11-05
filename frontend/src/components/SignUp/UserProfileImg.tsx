a350379a783b16c5ad470a1534a49aa88d621098
정예지 <wjddpwl1222@gmail.com>
2023-11-05 12:56:27 +0900

    [S09P31A302-197] feat: Add ProfileImgList in Modal of UserProfileImg Page

--- frontend/src/components/SignUp/UserProfileImg.tsx
+++ frontend/src/components/SignUp/UserProfileImg.tsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import './SignUp.scss'
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
+import { renderToString } from 'react-dom/server'
 import { userSignup } from '@/apis/member'
 
 const UserProfileImg = () => {
   const [inputProfileImg, setInputProfileImg] = useState<string>('')
+  const [isProfileImg, setIsProfileImg] = useState<boolean>(false)
   const MySwal = withReactContent(Swal)
 
   const saveUser = () => {
@@ -14,34 +16,59 @@ const UserProfileImg = () => {
   }
 
   const chooseProfileImg = (url: string) => {
+    console.log(url)
     setInputProfileImg(url)
+    setIsProfileImg(true)
   }
 
   const moveProfileImg = () => {
-    const content = (
-      <button onClick={() => chooseProfileImg('src/asset/profile/bear.jpg')}>
-        <img src="src/asset/profile/bear.jpg" alt="bear" />
-      </button>
-    )
+    const imgList: string[] = [
+      'bear',
+      'cat',
+      'frog',
+      'monkey',
+      'pig',
+      'rabit',
+      'rat',
+      'sheep',
+      'tiger',
+    ]
+    const content = imgList.map((img: string) => {
+      const reactComponent = (
+        <button
+          onClick={() => chooseProfileImg(`src/asset/profile/${img}.jpg`)}
+        >
+          <img
+            className="selectImg"
+            src={`src/asset/profile/${img}.jpg`}
+            alt={img}
+          />
+        </button>
+      )
+
+      const htmlString = renderToString(reactComponent)
+      return htmlString
+    })
 
     MySwal.fire({
       title: '당신의 원픽은?',
       html: content,
-      width: 600,
+      width: 800,
     })
   }
 
   return (
     <div className="w-8/12 h-3/5 relative">
-      <div className="bg-no-repeat w-full h-full bg-contain bg-[url('./asset/student_card/rm245-bb-17-g.jpg')]">
-        <button onClick={() => moveProfileImg()}>
-          {/* <input type="file" accept="image/*" onChange={handleFileUpload} /> */}
-          모달 띄웁니다!
+      <div className="flex bg-no-repeat w-full h-full bg-contain bg-[url('./asset/student_card/rm245-bb-17-g.jpg')]">
+        <button className="profileImg" onClick={() => moveProfileImg()}>
+          {inputProfileImg}
         </button>
-        <div className="rounded-full border-slate-950">학생</div>
-        <div>이름</div>
-        <div>나이</div>
-        <div>생일</div>
+        <span>
+          <div className="rounded-full border-slate-950">학생</div>
+          <div>이름</div>
+          <div>나이</div>
+          <div>생일</div>
+        </span>
       </div>
       <button className="absolute right-0" onClick={saveUser}>
         지금부터 숲을 가꿔보자!
