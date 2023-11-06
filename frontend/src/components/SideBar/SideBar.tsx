5c749ef1518caffbff7b2f77a5b37c10d6d6f32d
신기정 <467shin@gmail.com>
2023-11-06 13:08:38 +0900

    feat: Main page layout

---
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -0,0 +1,14 @@
+import './SideBar.css'
+
+const SideBar = () => {
+  return (
+    <>
+      <div className="post-it">home</div>
+      <div className="post-it">home</div>
+      <div className="post-it">home</div>
+      <div className="post-it">home</div>
+    </>
+  )
+}
+
+export default SideBar
