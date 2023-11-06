e654d8a9ea71d6d116f573921c73bd26cdf7fc9c
정예지 <wjddpwl1222@gmail.com>
2023-11-06 13:40:44 +0900

    [S09P31A302-248] feat: MyPage

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from 'react'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { FiPlusCircle } from 'react-icons/fi'
 import { UserData } from '@/types'
 import '@/styles/fontList.scss'
 import '@/styles/profile.scss'
+import './MyPageStyle.scss'
 
 const MyPage = () => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const [userData, setUserData] = useState<UserData>([])
+  const MySwal = withReactContent(Swal)
+
   useEffect(() => {
     const persistedState = localStorage.getItem('persist:user')
     if (persistedState) {
@@ -15,6 +21,40 @@ const MyPage = () => {
       setUserData(data)
     }
   }, [])
+
+  const moveProfileImg = () => {
+    const content = (
+      <div>
+        {imgList.map((img: string) => (
+          <button
+            onClick={() => chooseProfileImg(`src/asset/profile/${img}.jpg`)}
+          >
+            <img
+              className="selectImg"
+              src={`src/asset/profile/${img}.jpg`}
+              alt={img}
+            />
+          </button>
+        ))}
+      </div>
+    )
+
+    MySwal.fire({
+      title: '당신의 원픽은?',
+      html: content,
+      width: 800,
+      heightAuto: true,
+      didOpen: () => openModal(),
+      willClose: () => closeModal(),
+
+      showCancelButton: true,
+      confirmButtonText: '선택',
+      cancelButtonText: '취소',
+
+      reverseButtons: true,
+    })
+  }
+
   return (
     <div className="outer-box">
       <div
@@ -27,11 +67,16 @@ const MyPage = () => {
         }}
       >
         <div className="status">학생</div>
-        <img
-          src={userData.profileImage}
-          alt="유저이미지"
-          className="mypage-image-container"
-        />
+        <div className="profileImgBox">
+          <img
+            src={userData.profileImage}
+            alt="유저이미지"
+            className="mypage-image-container"
+          />
+          <button className="addImgBtn" onClick={() => moveProfileImg()}>
+            <FiPlusCircle class="plusCircle" />
+          </button>
+        </div>
         <div className="nanum mypage-nickname">{userData.nickname}</div>
         <button className="mypage-button">가정 통신문 보기</button>
         <button className="mypage-button">튜토리얼 다시 보기</button>
