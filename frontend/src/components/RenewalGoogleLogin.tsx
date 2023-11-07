3d380e794eed3bf9fa1e1e30a31e8125a25f5356
정예지 <wjddpwl1222@gmail.com>
2023-11-07 16:11:46 +0900

    [S09P31A302-253] fix: Modify SignUp Req

--- frontend/src/components/RenewalGoogleLogin.tsx
+++ frontend/src/components/RenewalGoogleLogin.tsx
@@ -3,23 +3,24 @@ import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import googleBtn from '../asset/login_btn/web_light_sq_na@4x.png'
 import { userLogin } from '@/apis/member'
-import { addOauthProvider } from '@/stores/features/signupSlice'
+import { addIdToken, addOauthProvider } from '@/stores/features/signupSlice'
 
 const RenewalGoogleLogin = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const googleSocialLogin = useGoogleLogin({
     onSuccess: async (codeResponse) => {
-      console.log(codeResponse)
+      console.log('응답', codeResponse)
       const data = {
         oauthProvider: 'GOOGLE',
-        idToken: codeResponse,
+        idToken: codeResponse.code,
       }
 
       const loginResult = await userLogin(data)
 
       if (loginResult) {
         dispatch(addOauthProvider('GOOGLE'))
+        dispatch(addIdToken(codeResponse.code))
         navigate('/signUp')
       } else {
         navigate('/main')
