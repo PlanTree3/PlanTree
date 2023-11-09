1f803f0ed37343752abcd5c5ffa03586c35bfdc9
정예지 <wjddpwl1222@gmail.com>
2023-11-09 16:04:54 +0900

    [S09P31A302-258] feat: 가정통신문 페이지 생성

---
+++ frontend/src/pages/NewsLetterPage.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from 'react'
+
+const NewsLetterPage = () => {
+  const [inputNewsLetters, setInputNewsLetters] = useState<string[]>()
+  return <div>가정통신문</div>
+}
+
+export default NewsLetterPage
