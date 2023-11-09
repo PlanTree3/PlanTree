6ecbe9bfbb0b9f56b8d6da5ab948185204f46359
정예지 <wjddpwl1222@gmail.com>
2023-11-09 13:50:33 +0900

    fix: Conflict 해결

--- frontend/src/components/SignUp/UserName.tsx
+++ frontend/src/components/SignUp/UserName.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addName } from '@/stores/features/signupSlice'
 
