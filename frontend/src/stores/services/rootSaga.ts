528cc2e928e0703e78f08d896eed39bc5337ad9d
정예지 <wjddpwl1222@gmail.com>
2023-11-09 10:37:25 +0900

    [S09P31A302-255] fix: Modify rootSaga

--- frontend/src/stores/services/rootSaga.ts
+++ frontend/src/stores/services/rootSaga.ts
@@ -4,6 +4,5 @@ import { watchTokenEvent } from '@/stores/services/tokenEventSaga'
 import { watchFetchUserData } from '@/stores/services/userSaga'
 
 export default function* rootSaga() {
-  yield all([watchTokenEvent()])
-  yield all([watchFetchUserData()])
+  yield all([watchTokenEvent(), watchFetchUserData()])
 }
