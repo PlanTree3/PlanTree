cbe9ee5273853dfff545858cce7c7873a60df802
김연재 <yjkim9195@gmail.com>
2023-11-12 23:57:48 +0900

    fix: 그룹 가입 수락, 거절 수정

--- frontend/src/pages/AdminGroupRequestPage.tsx
+++ frontend/src/pages/AdminGroupRequestPage.tsx
@@ -10,23 +10,42 @@ const AdminGroupRequestPage = () => {
   const location = useLocation();
   const groupName = location.state?.groupName || '';
 
+  const [page, setPage] = useState(1)
   const [currentPage, setCurrentPage] = useState(1)
   const [studentsData, setStudentsData] = useState<any>(null)
-  // const GroupsPerPage = 5
+
+  const studentsPerPage = 5;
+  const startIndex = (page - 1) * studentsPerPage;
+  const endIndex = startIndex + studentsPerPage;
+  const currentStudents = studentsData?.slice(startIndex, endIndex) || [];
+
+  const totalPages = studentsData
+  ? Math.ceil(studentsData.length / studentsPerPage)
+  : 0
+
+const pageNumbers = []
+for (let i = 1; i <= totalPages; i += 1) {
+  pageNumbers.push(i)
+}
+
+const changePage = (page: number) => {
+  setCurrentPage(page)
+}
 
 // 그룹 가입 수락 대기 리스트 조회
   const handleGroupRequestList = async () => {
     try {
       const response = await groupStudentsRequest(groupId)
       console.log('Response:', response)
+      setStudentsData(response.data.joinRequestList)
     } catch (error) {
       console.error('Error:', error)
     }
   }
 
   //그룹 가입 수락
-  const handleGroupAccept = async () => {
-    const data = { memberId: memberId }
+  const handleGroupAccept = async (studentId: string) => {
+    const data = { memberId: studentId }
     try {
       const response = await groupJoinAccept(data)
       console.log('Response:', response)
@@ -36,8 +55,8 @@ const AdminGroupRequestPage = () => {
   }
 
   //그룹 가입 거절
-  const handleGroupRefuse = async () => {
-    const data = { memberId: memberId }
+  const handleGroupRefuse = async (studentId: string) => {
+    const data = { memberId: studentId  }
     try {
       const response = await groupJoinRefuse(data)
       console.log('Response:', response)
@@ -51,14 +70,14 @@ const AdminGroupRequestPage = () => {
   }, [])
 
 
-  const handleNo = () => {
+  const handleNo = (studentId: string) => {
     console.log('nonono')
-    handleGroupRefuse()
+    handleGroupRefuse(studentId)
   }
 
-  const handleYes = () => {
+  const handleYes = (studentId: string) => {
     console.log('yes!!')
-    handleGroupAccept()
+    handleGroupAccept(studentId)
   }
 
   // const indexOfLastGroup = currentPage * GroupsPerPage
@@ -85,28 +104,41 @@ const AdminGroupRequestPage = () => {
           {groupName}의 대기 학생 목록
         </div>
         <div className="ml-4">
-          <Link to={`/adminGroupDetail/${groupId}`}>
+          <Link to={`/adminGroupDetail/${groupId}`} state={{ groupName: groupName }}>
             <Button label="그룹으로 돌아가기" className="gray" />
           </Link>
         </div>
       </div>
       <br />
       <div className="studentListBox">
+      {currentStudents.length > 0 ? (
+    currentStudents.map((student: any, index: number) => (
         <div>
           <div className="groupRequest">
-            <p className="groupInfo">1</p>
-            <p className="groupInfo">신기정</p>
+            <p className="groupInfo">{index + 1 + (currentPage - 1) * 5}</p>
+            <p className="groupInfo">{student.studentName}</p>
             <div className="flex justify-around">
               <Button
                 className="primary"
-                onClick={handleYes}
+                onClick={() => handleYes(student.studentId)}
                 label="수락하기"
               />
-              <Button className="red" onClick={handleNo} label="거절하기" />
+              <Button className="red" onClick={() => handleNo(student.studentId)} label="거절하기" />
             </div>
           </div>
           <hr style={{ background: 'black', height: 1, border: 0 }} />
         </div>
+         ))
+         ) : (
+           <p>현재 수락 대기중인 학생이 없습니다.</p>
+         )}
+      </div>
+      <div className="pagination">
+        {pageNumbers.map((number) => (
+          <button key={number} onClick={() => changePage(number)}>
+            {number}
+          </button>
+        ))}
       </div>
     </div>
   )
