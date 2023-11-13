6e50b24878592fd3de380b6e58637910e5d2e09c
정예지 <wjddpwl1222@gmail.com>
2023-11-13 17:27:27 +0900

    [S09P31A302-303] fix: tutorial

--- frontend/src/components/Tutorial/Tutorial3.tsx
+++ frontend/src/components/Tutorial/Tutorial3.tsx
@@ -2,9 +2,12 @@ import { ForestCard } from '..'
 
 const Tutorial3 = () => {
   return (
-    <div className="flex justify-center items-center max-w-[80%]">
-      <ForestCard forestName="숲을 만들어 보아요!" />
-    </div>
+    <>
+      <div className="flex justify-center items-center max-w-[80%]">
+        <ForestCard forestName="숲을 만들어 보아요!" />
+      </div>
+      <div>*그룹 기능 제공*</div>
+    </>
   )
 }
 
