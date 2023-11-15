38a693474ec7e6681787855287b57376d6d89750
정도현 <cirel31@gmail.com>
2023-11-16 01:23:29 +0900

    test: pwa 적용 테스트
    1. pwa 기본 설정 적용 테스트 진행
    2. 모바일 터치로 dnd 가능하게 라이브러리 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 // import eslintPlugin from 'vite-plugin-eslint'
 // eslintPlugin()
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
